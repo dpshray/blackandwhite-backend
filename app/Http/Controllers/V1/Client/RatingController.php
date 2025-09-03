00fa6b4bfc360afa28f3d0b81dc41ac4f6970d31
Soyamstha <shrestha.soyam111@gmail.com>
2025-09-03 18:01:27 +0545

    rating on product by user api

--- app/Http/Controllers/V1/Client/RatingController.php
+++ app/Http/Controllers/V1/Client/RatingController.php
@@ -18,20 +18,34 @@ class RatingController extends Controller
     {
         $rating=Rating::where('product_id',$product->id)->get();
         $average_rating =$rating->avg('rating');
+        $count=$rating->count();
         if(!$average_rating)
         {
             return $this->apiError('0 rating on this product');
         }
-        return $this->apiSuccess('product average rating ',['average rating'=>$average_rating]);
+        return $this->apiSuccess('product average rating ',[
+            'average rating'=>$average_rating,
+            'total_rating'=>$count,
+        ]);
     }
     function add(RatingRequest $request,Product $product)
     {
         $user=Auth::user();
+        $exists=Rating::where('product_id',$product->id)->where('user_id',$user->id)->get();
+        if($exists)
+        {
+            return $this->apiError('{$user->name} has already given rating on this product');
+        }
         $rating=Rating::create([
             'user_id'=>$user->id,
             'product_id'=>$product->id,
             'rating'=>$request->rating,
             'review'=>$request->review,
         ]);
+        if(!$rating)
+        {
+            return $this->apiError('failed to add rating');
+        }
+        return $this->apiSuccess('your rating has been added rating');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -79,8 +79,6 @@ Route::prefix('admin')->group(function () {
             Route::post('/update-product/{product}', 'update_product');
             Route::delete('/delete-product/{product}', 'delete_product');
             Route::post('/restore-product/{id}', 'restore_product');
-
-
         });
         Route::controller(BannerController::class)->group(function () {
             Route::post('/add-banner', 'add_banner');
