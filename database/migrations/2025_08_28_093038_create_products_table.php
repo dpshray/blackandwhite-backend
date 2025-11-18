0e4738a639b3d50d91632bd476d9a8f9a1c0493d
Soyamstha <shrestha.soyam111@gmail.com>
2025-11-18 14:25:14 +0545

    change

--- database/migrations/2025_08_28_093038_create_products_table.php
+++ database/migrations/2025_08_28_093038_create_products_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('slug');
-            $table->string('description');
+            $table->text('name');
+            $table->text('slug');
+            $table->text('description');
             $table->decimal('price',10,2);
             $table->decimal('discount_price',10,2)->nullable();
             $table->string('pattern');
