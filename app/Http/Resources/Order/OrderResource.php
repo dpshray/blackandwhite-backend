f823e0416c66a17e1e42cd5bee21e5baa69d4682
Soyamstha <shrestha.soyam111@gmail.com>
2025-12-02 17:12:09 +0545

    change

--- app/Http/Resources/Order/OrderResource.php
+++ app/Http/Resources/Order/OrderResource.php
@@ -22,15 +22,14 @@ class OrderResource extends JsonResource
             'status' => $this->status,
             'items' => $this->items->map(function ($item) {
                 return [
-                    'product_name' => $item->product->name ?? null,
-                    'variant_size' => $item->variant->size ?? null,
-                    'variant_color' => $item->variant->color ?? null,
-                    'quantity' => $item->quantity,
-                    'discount_price' => $item->product->discount_price ??null,
-                    'price'=>$item->product->price,
-                    'discount_percent'=>$item->product->discount_percent,
-                    'image' => $item->product?->getFirstMediaUrl(Product::MAIN_IMAGE) ?? null,
-
+                    'product_name' => $item?->product->name ?? null,
+                    'variant_size' => $item?->variant->size ?? null,
+                    'variant_color' => $item?->variant->color ?? null,
+                    'quantity' => $item?->quantity,
+                    'discount_price' => $item?->product->discount_price ??null,
+                    'price'=>$item?->product->price,
+                    'discount_percent'=>$item?->product->discount_percent,
+                    'image' => $item?->product?->getFirstMediaUrl(Product::MAIN_IMAGE) ?? null,
                 ];
             }),
         ];
