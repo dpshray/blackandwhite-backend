d34919649f0b656f1125e0a1fc046eb4c43d2d4c
Soyamstha <shrestha.soyam111@gmail.com>
2025-12-02 16:40:43 +0545

    change in wishlist

--- app/Http/Resources/Wishlist/WishlistResource.php
+++ app/Http/Resources/Wishlist/WishlistResource.php
@@ -22,29 +22,29 @@ class WishlistResource extends JsonResource
 
             // Product Info
             'product_id' => $this->product_id,
-            'title' => $this->product->name,
-            'slug' => $this->product->slug,
-            'price' => $this->product->price,
-            'discount_price' => $this->product->discount_price,
-            'discount_percent' => $this->product->discount_percent,
-            'images' => $this->product->getMedia(Product::MEDIA_NAME)->map(function ($media) {
+            'title' => $this->product?->name,
+            'slug' => $this->product?->slug,
+            'price' => $this->product?->price,
+            'discount_price' => $this->product?->discount_price,
+            'discount_percent' => $this->product?->discount_percent,
+            'images' => $this->product?->getMedia(Product::MEDIA_NAME)->map(function ($media) {
                 return $media->getUrl();
-            }),
-            'main_image'=>$this->product->getFirstMediaUrl(Product::MAIN_IMAGE),
+            }) ?? [],
+            'main_image' => $this->product?->getFirstMediaUrl(Product::MAIN_IMAGE),
             //category
-            'categories' => $this->product->categories->map(function ($cat) {
+            'categories' => $this->product?->categories->map(function ($cat) {
                 return [
                     'id' => $cat->id,
                     'slug' => $cat->slug,
                     'name' => $cat->title,
                 ];
-            }),
+            }) ?? [],
             // Variant Info
             'variant_id' => $this->variant_id,
             'variant' => [
-                'size' => $this->variant->size ?? null,
-                'color' => $this->variant->color ?? null,
-                'stock' => $this->variant->stock ?? null,
+                'size' => $this->variant?->size ?? null,
+                'color' => $this->variant?->color ?? null,
+                'stock' => $this->variant?->stock ?? null,
             ],
         ];
     }
